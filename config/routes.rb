ca69479cfd9cecc81ebbaf1d52806fcbdb743f6c
saipratap99 <122005067@sastra.ac.in>
2020-04-05 17:39:14 +0530

    After removing bad commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :todos
   patch "todos/complete/:id", to: "todos#complete", as: "todo_complete"
+  get "saipratap99/myusers", to: "todos#myusers"
   root to: "home#index"
 end
